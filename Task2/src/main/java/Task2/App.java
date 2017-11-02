15cbc3bf16bb1422bf16622fc7ea308cd7e518a6
stasdev <dvstas00@gmail.com>
2017-11-02 22:39:33 +0200

    model v.1.0

--- Task2/src/main/java/Task2/App.java
+++ Task2/src/main/java/Task2/App.java
@@ -1,5 +1,7 @@
 package Task2;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Model model = new Model();
     }
 }
