282321f862b996269dba2d0e3f59a24f8e6a19cf
stasdev <dvstas00@gmail.com>
2017-11-13 02:04:32 +0200

    Add exception

--- Task3/src/main/java/Task3/model/Model.java
+++ Task3/src/main/java/Task3/model/Model.java
@@ -1,56 +1,18 @@
 package Task3.model;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 /**
  * Created by dvsta on 09.11.2017.
  */
 public class Model {
-    String name, surname, nickName, phoneNumber, skype, email;
+    NoteBook noteBook = new NoteBook();
 
-    public String getName() {
-        return name;
+    public NoteBook getNoteBook() {
+        return noteBook;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getSkype() {
-        return skype;
-    }
-
-    public void setSkype(String skype) {
-        this.skype = skype;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setNoteBook(NoteBook noteBook) {
+        this.noteBook = noteBook;
     }
 }
